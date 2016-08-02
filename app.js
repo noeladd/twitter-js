931d31cec5b7c6c03744352ccbc307fb945c3a6c
Rachel Addleman <addlemanrachel@gmail.com>
2016-08-02 15:18:58 -0400

    changed app.js and package.json

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ app.listen(3000, function(){
 
 });
 
+
+app.use(function(req, res, next){
+    console.log(req.method, req.originalUrl, res.statusCode);
+    next();
+});
+
+app.use('/special', function(req, res, next){
+  console.log("You've reached a special page");
+  next();
+});
+
 app.get('/', function(req, res){
-  res.send("Welcome to our page!");
+  res.send('Welcome to our page!');
+});
+
+app.get('/news', function (req, res){
+  res.send('This is the news page!');
 });
